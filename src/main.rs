768c491db8df2cb50347a5a121d57dffd00296ba
NorbertCseh <norbert.cseh@protonmail.com>
2023-04-07 14:39:08 +0200

    hello stuff

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,40 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(HelloPlugin)
+        .run();
+}
+
+fn hello_world() {
+    println!("hello world");
+}
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("egyes".to_string())));
+    commands.spawn((Person, Name("kettes".to_string())));
+    commands.spawn((Person, Name("harmas".to_string())));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in &query {
+        println!("hello {}!", name.0);
+    }
+}
+
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_startup_system(add_people)
+            .add_system(hello_world)
+            .add_system(greet_people);
+    }
 }
