3342de0ed14e6b0b7dda9eb3dd51bfe09909d503
Norbert Istvan Cseh <ncseh@pop-os.localdomain>
2023-04-08 10:31:33 +0200

    timer

--- src/main.rs
+++ src/main.rs
@@ -23,18 +23,24 @@ fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("harmas".to_string())));
 }
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in &query {
-        println!("hello {}!", name.0);
-    }
-}
-
 pub struct HelloPlugin;
 
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(add_people)
-            .add_system(hello_world)
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
+            .add_startup_system(add_people)
+            // .add_system(hello_world)
             .add_system(greet_people);
+    }
+}
+
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("hello {}!", name.0);
+        }
     }
 }
